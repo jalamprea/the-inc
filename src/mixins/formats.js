fe8f5ffa5b1181832e36f296abbb4d4c46da7e0d
Julian Lamprea <jalamprea@gmail.com>
2020-02-22 15:41:40 -0500

    Added Inventory table with products stock

---
+++ src/mixins/formats.js
@@ -0,0 +1,10 @@
+const formatsMixin = {
+  methods: {
+    formatCurrency: function (value) {
+      const val = (value / 1).toFixed(0).replace('.', ',')
+      return '$' + val.toString().replace(/\B(?=(\d{3})+(?!\d))/g, '.')
+    }
+  }
+}
+
+export default formatsMixin
--- src/pages/Inventory.vue
+++ src/pages/Inventory.vue
@@ -1,11 +1,56 @@
 <template>
   <q-page padding>
     <!-- content -->
+    <section class="q-ma-md">
+      <div v-if="hasProducts" class="products-wrapper">
+
+        <q-table
+          title="Inventario de Productos"
+          :data="products"
+          :columns="columns"
+          row-key="name"
+        />
+
+      </div>
+      <div
+        v-else
+      >
+        No hay productos en bodega
+      </div>
+    </section>
+
   </q-page>
 </template>
 
 <script>
+import formatMixin from '../mixins/formats'
 export default {
-  // name: 'PageName',
+  computed: {
+    hasProducts () {
+      console.log('hasProducts', this)
+      return this.products && this.products.length > 0
+    }
+  },
+  data () {
+    return {
+      columns: [
+        { name: 'name', label: 'Producto', field: 'name', align: 'left' },
+        { name: 'stock', label: 'Disponibilidad', field: 'stock', align: 'center' },
+        { name: 'price', label: 'Precio', field: 'price', format: val => `${this.formatCurrency(val)}` },
+        { name: 'tax', label: 'IVA', field: 'tax', format: val => `${val * 100}%` }
+      ],
+      products: []
+    }
+  },
+  mixins: [formatMixin],
+  firestore () {
+    const company = this.$store.getters['company/getActiveCompany']
+    console.log('Comp:', company)
+    return {
+      products: this.$firestore
+        .collection('companies').doc(company)
+        .collection('inventory')
+    }
+  }
 }
 </script>
