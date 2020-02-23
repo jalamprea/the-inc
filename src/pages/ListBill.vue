afa4fc5f4c5377e40c7c64804fe28f9e84e8222e
Leo Prada <leo@ziotag.com>
2020-02-23 14:41:13 -0500

    Bind Bills

--- src/pages/ListBill.vue
+++ src/pages/ListBill.vue
@@ -13,20 +13,20 @@
             v-for="(bill, index) in bills"
             :key="index"
             clickable
-            @click="openBill(bill)"
           >
             <!-- <q-item-section v-if="!bill">
               <q-skeleton type="rect" />
             </q-item-section> -->
-            <q-item-section side>
+            <q-item-section @click="openBill(bill)" side>
               <q-icon name="receipt"></q-icon>
             </q-item-section>
-            <q-item-section>
-              <q-item-label lines="1">{{ bill._id_ }}</q-item-label>
+            <q-item-section @click="openBill(bill)">
+              <q-item-label lines="1">{{ bill.id }}</q-item-label>
               <q-item-label caption>{{bill.created_at.toDate()}}</q-item-label>
             </q-item-section>
             <q-item-section side>
               {{ translateStatus(bill.status) }}
+              <q-btn @click="deleteDraftBill(bill.id)" v-if="bill.status === 'draft'" icon="delete" dense flat></q-btn>
             </q-item-section>
           </q-item>
         </q-list>
@@ -52,10 +52,10 @@ export default {
     openBill (bill) {
       switch (bill.status) {
         case 'processed':
-          this.$router.push(`/factura/${bill._id_}`)
+          this.$router.push(`/factura/${bill.id}`)
           break
         case 'draft':
-          this.$router.push(`/crear/${bill._id_}`)
+          this.$router.push(`/crear/${bill.id}`)
           break
         default:
           break
@@ -74,24 +74,22 @@ export default {
     getBills () {
       const company = this.$store.getters['company/getActiveCompany']
       const store = this.store
-      this.$firestore
+      const dbRef = this.$firestore
         .collection('companies').doc(company)
         .collection('stores').doc(store)
         .collection('invoices').orderBy('created_at', 'desc')
-        .get().then(snapshot => {
-          const empty = new Array(snapshot.size)
-          this.bills = empty
-          const bills = []
-          snapshot.forEach(doc => {
-            const parsed = doc.data()
-            parsed._id_ = doc.id
-            bills.push(parsed)
-          })
-          // setTimeout(() => {
-          //   this.bills = bills
-          // }, 3000)
-          this.bills = bills
-        })
+
+      this.$bind('bills', dbRef).then(res => {
+        console.log('bills :', res)
+      })
+    },
+    deleteDraftBill (id) {
+      const company = this.$store.getters['company/getActiveCompany']
+      const store = this.store
+      this.$firestore
+        .collection('companies').doc(company)
+        .collection('stores').doc(store)
+        .collection('invoices').doc(id).delete()
     }
   },
   mounted () {
